dd345b3429d8017e93c00b1d7d97a4029e3ed066
Yakov <yakov6091@gmail.com>
2026-07-21 21:56:13 +0300

    Add a login user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,8 +2,11 @@ import { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const dbPath = path.join(__dirname, "../../db/db.json");
+// A temporary secret key (In a real app, this goes in your .env file!)
+const JWT_SECRET = "super_secret_temporary_key_12345";
 
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
     // res.status(200).json({ message: "Hello from backend registration!" });
@@ -58,4 +61,56 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
         console.error("Registration error:", error);
         res.status(500).json({ message: "Internal server error during registration" });
     }
-}
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            res.status(400).json({ message: "Username and password are required." });
+            return;
+        }
+
+        // Read database
+        const dbData = fs.readFileSync(dbPath, "utf-8");
+        const db = JSON.parse(dbData);
+
+        // Find user
+        const user = db.users.find((user: any) => user.username.toLowerCase() === username.toLowerCase());
+        if (!user) {
+            res.status(401).json({ message: "Invalid username or password" });
+            return;
+        };
+
+        // Check if password is correct
+        // bcrypt.compare checks if the plain-text password matches the encrypted hash in db.json
+        const isPasswordCorret = await bcrypt.compare(password, user.hashedPassword);
+        if (!isPasswordCorret) {
+            res.status(401).json({ message: "Invalid username or password." });
+            return;
+        };
+
+        // Generate the JWT (The VIP wristband) 
+        // We put the user's ID inside the payload so we know who they are later
+        const token = jwt.sign(
+            { userId: user.id, username: user.username },
+            JWT_SECRET,
+            { expiresIn: "1h" } // Wristband expires in 1 hour
+        );
+
+        // Respond with the token and user details
+        res.status(200).json({
+            message: "Login successful!",
+            token, // The frontend will save this
+            user: {
+                id: user.id,
+                username: user.username
+            }
+        });
+
+    } catch (error) {
+        console.error("Login error:", error)
+        res.status(500).json({ message: "Internal server error during login." });
+    }
+};
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller";
+import { registerUser, loginUser } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 // export default router
 
